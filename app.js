dad7fb0392402b0927f1d2cbf4a8c79e036e393e
damianarp <damianarp@gmail.com>
2021-10-25 01:20:12 -0300

    express.json() + GET and POST product

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 // Importaciones necesarias.
 const express = require('express');
+require('dotenv/config');
+
+// Definimos una variable de ambiente para /api/v1 del archivo .env.
+const api = process.env.API_URL;
 
 // Creamos la instancia de la aplicación con Express.
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello API')
+// Configuramos Express para que trabaje con datos de tipo JSON y con el middleware urlencoded.
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// http://localhost:3000/api/v1/products
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url'
+    }
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 })
 
 ////////// CONFIGURACIÓN DEL PUERTO DE EXPRESS //////////
