afa2befbe57c169a3040dc4f3ca51771943445ce
damianarp <damianarp@gmail.com>
2021-10-27 22:44:20 -0300

    POST nuevo user.

--- routers/users.js
+++ routers/users.js
@@ -18,5 +18,29 @@ router.get(`/`, async (req, res) => {
     res.send(userList);
 });
 
+////////// HTTP REQUEST POST //////////
+// Manejamos el post con un async-await.
+router.post('/', async (req, res) => {
+    // Creamos una instancia de User y la llenamos con la data del modelo.
+    let user = new User({
+        name        : req.body.name,
+        email       : req.body.email,
+        passwordHash: req.body.passwordHash,
+        phone       : req.body.phone,
+        isAdmin     : req.body.isAdmin,
+        street      : req.body.street,
+        apartment   : req.body.apartment,
+        zip         : req.body.zip,
+        city        : req.body.city,
+        country     : req.body.country
+    })
+    // Guardamos el usuario en la BD y comprobamos si se produce algún error.
+    user = await user.save();
+    // Si se produce algún error.
+    if(!user) return res.status(500).send('The user cannot be created.');
+    // Si no se produce ningún error.
+    res.status(200).send(user);
+});
+
 // Exportamos el módulo
 module.exports = router;
